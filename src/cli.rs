192de28ee4c72ddb5bccffb604e5d2f951c14b1f
Missael Gastelum <missaga@proton.me>
2024-09-07 20:00:42 -0700

    separate cli parser logic

---
+++ src/cli.rs
@@ -0,0 +1,116 @@
+use clap::{ArgGroup, Args, Subcommand};
+use std::path::PathBuf;
+use strum::{Display, EnumIter};
+
+#[derive(Debug, clap::Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Parser {
+    #[command(subcommand)]
+    pub command: Command,
+    /// Quiet mode. Causes most warning and diagnostic messages to be suppressed.
+    #[arg(short, long, global = true, conflicts_with = "verbose")]
+    pub quiet: bool,
+    /// Verbose mode. Causes asd to print debugging messages about its progress.
+    #[arg(short, long, global = true, conflicts_with = "quiet")]
+    pub verbose: bool,
+}
+
+#[derive(Debug, Subcommand, EnumIter, Display)]
+pub enum Command {
+    /// Open SSH connection to given remote [default]
+    Ssh(ConnectionArgs),
+    /// Open SFTP connection to given remote
+    Sftp(ConnectionArgs),
+    /// Execute script or commands on listed remotes
+    Exec(ExecuteArgs),
+    /// Send list of files to the specified remotes
+    Put(FileArgs),
+    /// Get list of files to the specified remotes
+    Get(FileArgs),
+    /// Execute ansible playbook using asd password detection
+    Book(PlaybookArgs),
+    /// Configure application
+    #[command(subcommand)]
+    Config(Config),
+}
+
+#[derive(Debug, Args, Default)]
+pub struct ConnectionArgs {
+    /// Remote to connect to
+    pub remote: String,
+    /// Ask for connection password
+    #[arg(short = 'k', long)]
+    pub ask_pass: bool,
+    /// Disabled password renewal (and connectivity test in case of --dry-run)
+    #[arg(short, long)]
+    pub cache: bool,
+    /// Force password renewal, invalidating cache
+    #[arg(short, long)]
+    pub force: bool,
+    /// Do not connect to the remote; merely test the connection
+    #[arg(short = 'u', long)]
+    pub dry_run: bool,
+    /// Print password (implies --dry-run)
+    #[arg(short, long)]
+    pub print: bool,
+}
+
+#[derive(Debug, Args, Default)]
+#[clap(group( ArgGroup::new("required") .required(true)))]
+#[clap(group( ArgGroup::new("exec") .required(false)))]
+pub struct ExecuteArgs {
+    /// Specify inventory host path or comma separated host list
+    #[arg(short, long)]
+    pub inventory: String,
+    /// Script path to execute in the remote
+    #[arg(
+        short = 'x',
+        long,
+        value_name = "FILE",
+        group = "required",
+        group = "exec"
+    )]
+    pub execute: Option<PathBuf>,
+    /// Args to be passed to remote script
+    #[clap(short, long, allow_hyphen_values = true, requires = "exec")]
+    pub args: Option<Vec<String>>,
+    /// Commands to send to the remote
+    #[arg(short, long, allow_hyphen_values = true, group = "required")]
+    pub commands: Option<Vec<String>>,
+}
+
+#[derive(Debug, Args, Default)]
+pub struct FileArgs {
+    /// Specify inventory host path or comma separated host list
+    #[arg(short, long)]
+    pub inventory: String,
+    /// Files to send to the remotes
+    pub files: Vec<PathBuf>,
+}
+
+#[derive(Debug, Args, Default)]
+pub struct PlaybookArgs {
+    /// Specify inventory host path or comma separated host list
+    #[arg(short, long)]
+    pub inventory: String,
+    /// Args to be passed to ansible, must be specified after --inventory
+    #[clap(trailing_var_arg = true, allow_hyphen_values = true)]
+    pub ansible_args: Vec<String>,
+}
+
+#[derive(Debug, Subcommand, Default)]
+pub enum Config {
+    /// Initialize configuration and create neccesary folders
+    #[default]
+    Init,
+    /// Create/modify specified credentials
+    Credentials { user: Option<String> },
+    /// Set/change passphrase (use migrate if you didn't migrate manually)
+    Passphrase,
+    /// Migrate files with previous password to a new password
+    Migrate,
+    /// Open config file
+    File,
+    /// Reset config file to the base configuration
+    Reset,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,8 @@
-use clap::{ArgGroup, Args, Parser, Subcommand};
+mod cli;
+
+use cli::{Parser, Command, Config};
 use std::env;
-use std::path::PathBuf;
-use strum::{Display, EnumIter, IntoEnumIterator};
-
-#[derive(Debug, Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    pub command: Command,
-    /// Quiet mode. Causes most warning and diagnostic messages to be suppressed.
-    #[arg(short, long, global = true, conflicts_with = "verbose")]
-    quiet: bool,
-    /// Verbose mode. Causes asd to print debugging messages about its progress.
-    #[arg(short, long, global = true, conflicts_with = "quiet")]
-    verbose: bool,
-}
-
-#[derive(Debug, Subcommand, EnumIter, Display)]
-enum Command {
-    /// Open SSH connection to given remote [default]
-    Ssh(ConnectionArgs),
-    /// Open SFTP connection to given remote
-    Sftp(ConnectionArgs),
-    /// Execute script or commands on listed remotes
-    #[clap(group(
-        ArgGroup::new("required")
-            .required(true)
-    ))]
-    #[clap(group(
-        ArgGroup::new("exec")
-            .required(false)
-    ))]
-    Exec {
-        /// Specify inventory host path or comma separated host list
-        #[arg(short, long)]
-        inventory: String,
-        /// Script path to execute in the remote
-        #[arg(
-            short = 'x',
-            long,
-            value_name = "FILE",
-            group = "required",
-            group = "exec"
-        )]
-        execute: Option<PathBuf>,
-        /// Args to be passed to remote script
-        #[clap(short, long, allow_hyphen_values = true, requires = "exec")]
-        args: Option<Vec<String>>,
-        /// Commands to send to the remote
-        #[arg(short, long, allow_hyphen_values = true, group = "required")]
-        commands: Option<Vec<String>>,
-    },
-    /// Send list of files to the specified remotes
-    Put(FileArgs),
-    /// Get list of files to the specified remotes
-    Get(FileArgs),
-    /// Execute ansible playbook using asd password detection
-    Book {
-        /// Specify inventory host path or comma separated host list
-        #[arg(short, long)]
-        inventory: String,
-        /// Args to be passed to ansible, must be specified after --inventory
-        #[clap(trailing_var_arg = true, allow_hyphen_values = true)]
-        ansible_args: Vec<String>,
-    },
-    /// Configure application
-    #[command(subcommand)]
-    Config(ConfigEnum),
-}
-
-#[derive(Debug, Subcommand, Default)]
-enum ConfigEnum {
-    /// Initialize configuration and create neccesary folders
-    #[default]
-    Init,
-    /// Create/modify specified credentials
-    Credentials {
-        user: Option<String>,
-    },
-    /// Set/change passphrase (use migrate if you didn't migrate manually)
-    Passphrase,
-    /// Migrate files with previous password to a new password
-    Migrate,
-    /// Open config file
-    File,
-    /// Reset config file to the base configuration
-    Reset,
-}
-
-#[derive(Debug, Args, Default)]
-struct ConnectionArgs {
-    /// Remote to connect to
-    remote: String,
-    /// Ask for connection password
-    #[arg(short = 'k', long)]
-    ask_pass: bool,
-    /// Disabled password renewal (and connectivity test in case of --dry-run)
-    #[arg(short, long)]
-    cache: bool,
-    /// Force password renewal, invalidating cache
-    #[arg(short, long)]
-    force: bool,
-    /// Do not connect to the remote; merely test the connection
-    #[arg(short = 'u', long)]
-    dry_run: bool,
-    /// Print password (implies --dry-run)
-    #[arg(short, long)]
-    print: bool,
-}
-
-#[derive(Debug, Args, Default)]
-struct FileArgs {
-    /// Specify inventory host path or comma separated host list
-    #[arg(short, long)]
-    inventory: String,
-    /// Files to send to the remotes
-    files: Vec<PathBuf>,
-}
+use strum::IntoEnumIterator;
 
 fn main() {
     let mut args = env::args().collect::<Vec<_>>();
@@ -131,7 +17,25 @@ fn main() {
             args.insert(1, "ssh".to_string());
         }
     }
-    let cli = Cli::parse_from(args);
+    let cli: Parser = clap::Parser::parse_from(args);
+
+    #[allow(unused)]
+    match &cli.command {
+        Command::Ssh(args) => {}
+        Command::Sftp(args) => {}
+        Command::Put(args) => {}
+        Command::Get(args) => {}
+        Command::Exec(args) => {}
+        Command::Book(args) => {}
+        Command::Config(command) => match command {
+            Config::Init => {}
+            Config::File => {}
+            Config::Reset => {}
+            Config::Migrate => {}
+            Config::Passphrase => {}
+            Config::Credentials { user } => {}
+        },
+    }
 
     print!("{cli:#?}");
 }
